3656c62c368cbf7a434ab29e015dbe098b28481a
DURUII <wust.durui@qq.com>
2025-03-09 00:57:55 +0800

    Update ieee.py

--- utils/ieee.py
+++ utils/ieee.py
@@ -21,6 +21,7 @@ def setup_driver():
         WebDriver: A new instance of Selenium WebDriver.
     """
     chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument("--headless=new")
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
